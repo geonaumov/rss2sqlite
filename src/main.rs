7574c71127516a883b173745a09cbfc3d5e4b4fd
Geo. Naumov <george.naumov@outlook.com>
2024-10-20 22:46:36 +0300

    Add command line interface and refactor config handling
    Introduced command line interface using Clap to handle update and last subcommands. Refactored configuration loading to use Confy and separated configurations into a new module. Enhanced error handling and debugging capabilities.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,21 @@
-// RSS to sqlite database collector
-
 mod modules {
     pub mod sql;
+    pub mod config;
 }
 
 use modules::sql::create_db;
 use modules::sql::insert_feed_items;
 use modules::sql::last;
+use modules::config::Config;
 
+use std::error::Error;
+use clap::{Arg, Command};
 use anyhow::{Context, Result};
+use env_logger;
 use env_logger::Builder;
-use log::{debug, info, warn, LevelFilter};
+use log::{debug, error, info, warn, LevelFilter};
 use reqwest::blocking;
 use rss::Channel;
-use serde::Deserialize;
-// use std::error::Error;
-use std::fs;
-
-#[derive(Deserialize)]
-struct FeedConfig {
-    dbfile: String,
-    feeds: Vec<String>,
-}
-
-fn load_config() -> Result<FeedConfig> {
-    debug!("Loading configuration");
-    let config_data =
-        fs::read_to_string("config.json").context("Failed to read configuration file")?;
-    debug!("Parsing configuration");
-    let config: FeedConfig =
-        serde_json::from_str(&config_data).context("Failed to parse configuration file")?;
-    Ok(config)
-}
 
 fn fetch_rss_feed(url: &str) -> Result<Channel> {
     debug!("Fetching RSS feed {}", url);
@@ -40,22 +24,64 @@ fn fetch_rss_feed(url: &str) -> Result<Channel> {
     Ok(channel)
 }
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn Error>> {
+    // Setup logger
     let mut builder = Builder::from_default_env();
     builder.filter_level(LevelFilter::Info);
     builder.init();
-    // let's go
-    info!("RSS feed collector");
-    warn!("Work in progress");
-    let config = load_config()?;
-    let conn = create_db(&config.dbfile)?;
-    info!("Processing {} feeds", config.feeds.len());
-    for feed in config.feeds.iter() {
-        let parsed_feed = fetch_rss_feed(feed).context("Failed to parse feed")?;
-        info!("{} items from {}", parsed_feed.items().len(), feed);
-        insert_feed_items(&conn, &parsed_feed).context("Failed to insert record")?;
+
+    // Setup command arguments
+    let matches = Command::new("RSS to SQLite")
+        .version("0.2.0")
+        .author("Geo Naumov <george.naumov@outlook.com>")
+        .about("A collector for RSS feeds written in Rust. Uses the SQLite database.")
+        .subcommand(Command::new("update").about("Update the feeds"))
+        .subcommand(Command::new("last").about("Print last news items"))
+        .arg(
+            Arg::new("debug")
+                .help("Turn on debugging information")
+                .short('d')
+                .long("debug")
+                .action(clap::ArgAction::SetTrue),
+        )
+        .get_matches();
+
+    // Debug flag
+    if matches.get_flag("debug") {
+        println!("Debugging mode is on");
     }
-    let _ = last(&conn).context("Failed to query the database")?;
-    info!("Finished");
+
+    // Match subcommands
+    match matches.subcommand() {
+        Some(("update", _)) => {
+            info!("Updating feeds...");
+            // Load configuration
+            debug!("Loading configuration");
+            let config: Config = confy::load("RSS2SQLite", None)?;
+            let conn = create_db(&config.dbfile);
+            info!("Processing {} feeds", config.feeds.len());
+            for feed in config.feeds.iter() {
+                let parsed_feed = fetch_rss_feed(feed).context("Failed to parse feed");
+                info!("Feed: {}", feed);
+                match &conn {
+                    Ok(ref conn) => {
+                        insert_feed_items(&conn, &parsed_feed.unwrap()).context("Failed to insert record")?;
+                    },
+                    Err(..) => {
+                        // return Err(anyhow::anyhow!("Failed to open database: {}", e));
+                        error!("Database error!");
+                    }
+                }
+            }
+        }
+        Some(("last", _)) => {
+            info!("Last news feeds...");
+            let config: Config = confy::load("RSS2SQLite", None)?;
+            let conn = create_db(&config.dbfile);
+            let _ = last(&conn.unwrap()).context("Failed to query the database")?;
+        }
+        _ => warn!("Subcommand is required"),
+    }
+    info!("Finished!");
     Ok(())
 }
